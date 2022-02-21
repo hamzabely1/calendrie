3c7002676f21015394ced240004056106ef018e5
hamzabely1 <96245936+hamzabely1@users.noreply.github.com>
2022-02-21 13:59:28 +0100

    hi

--- src/index.js
+++ src/index.js
@@ -1,114 +1,82 @@
+const date = new Date();
 
-let jour = document.getElementById("jour")
-let moi = document.getElementById("moi")
-let time = document.getElementById("time")
-let evenment = document.createElement("div")
-let div = document.createElement("div")
-let body = document.getElementById("body")
-let prossimo = document.getElementById("prossima")
-let input = document.createElement("input")
-let num = 32
-let mois = ["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"]
+const renderCalendar = () => {
+  date.setDate(1);
 
+  const monthDays = document.querySelector(".days");
 
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
 
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
 
-for (let i = 1; i < num; i++) {
-jour.innerHTML += `<div id = jours> ${i} </div>`
-}
-jour.addEventListener("click",modal)
+  const firstDayIndex = date.getDay();
 
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
 
+  const nextDays = 7 - lastDayIndex - 1;
 
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-function modal() {
-    body.appendChild(evenment)
-    evenment.style.border ="solid 2px black"
-    evenment.style.height="100px"
-    evenment.style.width ="100px"
-    evenment.style.borderRadius ="10px"
-    evenment.style.position ="absolute"
-    evenment.style.top = "300px"
-    evenment.style.left ="100px"
-    evenment.style.background="white"
-    evenment.appendChild(input)
-    input.style.height = "95px"
-    input.style.width ="95px"
-    input.style.borderRadius ="10px"
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-}
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
 
+  let days = "";
 
-moi.innerHTML = mois[0]
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
 
-
-let day = new Date()
-time.innerHTML = day.getFullYear() ,day.getDate
-
-
-
-
-let m = 0
-
-if (m == 2)  {
-    alert("capo")
-}
-
-prossimo.addEventListener("click",pro)
-precedente.addEventListener("click",pre)
-
-
-function pro() {
-    m +=1;
-    moi.innerHTML = mois[m];
-
-
-    if (m == 1) {
-        jour.innerHTML = ""
-        num = 30
-        for (let i = 1; i < num; i++) {
-            jour.innerHTML += `<div id = jours> ${i} </div>`
-        }
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
     }
-    if (m == 0) {
-        jour.innerHTML = ""
-        num = 32
-        for (let i = 1; i < num; i++) {
-            jour.innerHTML += `<div id = jours> ${i} </div>`
-        }
-    }
-if (m == 2) {
-    
-}
+  }
 
-}
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
 
-function pre() {
-    m -=1;
-    moi.innerHTML = mois[m];
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
 
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
 
-    if (m == 1) {
-       
-        num = 30
-        jour.innerHTML = ""
-        for (let i = 1; i < num; i++) {
-            jour.innerHTML += `<div id = jours> ${i} </div>`
-        }
-    }
-if (m == 3) {
-    num = 30
-    jour.innerHTML = ""
-    for (let i = 1; i < num; i++) {
-        jour.innerHTML += `<div id = jours> ${i} </div>`
-    }
-}
-if (m == 0) {
-    jour.innerHTML = ""
-    num = 32
-    for (let i = 1; i < num; i++) {
-        jour.innerHTML += `<div id = jours> ${i} </div>`
-    }
-}
-}
-
+renderCalendar();
